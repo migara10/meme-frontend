cf6b2e1e29f05455a068a7cb2f461a2636ae37e5
migara tennakoon <migaraten@gmail.com>
2024-08-24 20:18:54 +0300

    add x

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,12 @@ import Dicord from "../assets/discord.png"
 import Facebook from "../assets/facebook.png"
 import Instagram from "../assets/instagram.png"
 import Logo from "../assets/memelogonav.png"
+import X from "../assets/x.png"
 import { resourcesLinks, platformLinks, communityLinks } from "../constants1"
 
 const Footer = () => {
   return (
-    <footer className="py-10 mt-20 border-t border-neutral-700 mx-auto max-w-screen-xl">
+    <footer className="mx-auto mt-20 max-w-screen-xl border-t border-neutral-700 py-10">
       <div className="grid grid-cols-2 gap-4 px-4 sm:grid-cols-2 sm:px-8 lg:grid-cols-4 lg:px-14">
         <div>
           <h3 className="sm:text-md mb-4 text-sm font-semibold">Resources</h3>
@@ -65,6 +66,18 @@ const Footer = () => {
             </span>
           </div>
 
+          <div className="mt-5 flex flex-shrink-0 items-center">
+            <a href="https://x.com/memescoin?s=21&t=UEB_WMtZkqwNoNKZifsE4A">
+              <img
+                className="mr-2 h-5 w-5 sm:h-7 sm:w-7"
+                src={X}
+                alt="MEME_Generate"
+              />
+              <span className="ml-2 text-sm tracking-tight sm:ml-3 sm:text-lg">
+                X
+              </span>
+            </a>
+          </div>
           <div className="mt-5 flex flex-shrink-0 items-center">
             <img
               className="mr-2 h-5 w-5 sm:h-7 sm:w-7"
